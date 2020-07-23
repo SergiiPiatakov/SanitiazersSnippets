08d9211ca81c63e480d6576a91888b8ed73626d8
Sergii Piatakov <sergii.piatakov@globallogic.com>
2020-07-23 12:00:16 +0300

    add example with function call of invalid type
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- 001_main.cpp
+++ 001_main.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
 
+void fun (float f) {}
+
 int main (int argc, char ** argv)
 {
     unsigned char i = -1;
@@ -7,6 +9,9 @@ int main (int argc, char ** argv)
     unsigned char c = data [i];
 
     float f = c / (argc - 1);
+
+    void (* fptr)(int) = reinterpret_cast <void (*)(int)> (& fun);
+    fptr (42);
 
     return EXIT_SUCCESS;
 }
